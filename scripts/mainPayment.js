103a1584377b47ac981a347be5c246a721b5b624
matthewjgarcia <matthewjgarcia@csu.fullerton.edu>
2021-03-14 23:17:21 -0700

    Initial commit

---
+++ scripts/mainPayment.js
@@ -0,0 +1,15 @@
+(function (window) {
+    'use strict';
+    var FORM_SELECTOR = '[data-payment="form"]';
+    var App = window.App;
+    var Payment = App.Payment;
+    var DataStore = App.DataStore;
+    var FormHandler = App.FormHandler;
+   // var formHandler = new FormHandler(FORM_SELECTOR);
+    //formHandler.addSubmitHandler(myPayment.createOrder.bind(myPayment));
+    //console.log(formHandler);
+    // I attempted to set this up similar to main.js to be used with the payment form but could not 
+    //get it working properly when assigning formHandler
+})(window);
+
+// edit this for payment
